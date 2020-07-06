71a54d135e41ecccd24cef1e4a38719f539e75f9
elmonsomiat <asolaguren33@hotmail.com>
2020-07-06 20:02:32 +0100

    moved out get dims funcs

--- optimizer/table_organizer.py
+++ optimizer/table_organizer.py
@@ -1,11 +1,10 @@
 from utils.utils import calculate_rectangles
-from utils.draw_utils import create_diagram
+from utils.draw_utils import create_diagram, get_room_dims, get_security_dist, get_table_dims
 from rectpack import newPacker
 
 
 
 def run(room_size_split, security_distance, table_sizes, draw=True):
-    
     for room_size in room_size_split:
         _opt_room_size = [dim + security_distance for dim in room_size]
         rectangles = calculate_rectangles(_opt_room_size, security_distance, table_sizes)
@@ -20,34 +19,6 @@ def run(room_size_split, security_distance, table_sizes, draw=True):
             for box in packer[0]:
                 print(box)
 
-
-def get_room_dims(loop):
-    _len = input(f'What is the lenght of space {loop}?: ')
-    _wid = input(f'What is the width of space {loop}?: ')
-    try:
-        return [float(_len), float(_wid)]
-        restaurant_list.append([float(_len), float(_wid)])
-    except ValueError:
-        print('Dimensions should be numbers! Try again...')
-        get_room_dims(loop)
-
-def get_security_dist():
-    try:
-        security = input('What is the security distance?: ')
-        return float(security)
-    except ValueError:
-        print('Dimensions should be numbers! Try again...')
-        get_security_dist()
-
-
-def get_table_dims():
-    try:
-        _len = input('What is the lenght of the table?: ')
-        _wid = input('What is the width of the table?: ')
-        return [float(_len), float(_wid)]
-    except ValueError:
-        print('Dimensions should be numbers! Try again...')
-        get_table_dims()
 
 if __name__=='__main__':
     print('Welcome to the restaurant optimiser!')
--- optimizer/utils/utils.py
+++ optimizer/utils/utils.py
@@ -1,6 +1,7 @@
 def resize_rectangles(list_rectangles, safety_dist):
     return [[item + safety_dist for item in rectangle]  for rectangle in list_rectangles]
 
+
 def repeat_rectangles(list_rectangles, box):
     output_list = []
     for rectangle in list_rectangles:
@@ -11,8 +12,36 @@ def repeat_rectangles(list_rectangles, box):
     return output_list
 
 
-
 def calculate_rectangles(room_size, security_distance, table_sizes):
     options = resize_rectangles(table_sizes, security_distance)
     rectangles = repeat_rectangles(options, room_size)[0:-3]
     return rectangles
+
+
+def get_room_dims(loop):
+    _len = input(f'What is the lenght of space {loop}?: ')
+    _wid = input(f'What is the width of space {loop}?: ')
+    try:
+        return [float(_len), float(_wid)]
+        restaurant_list.append([float(_len), float(_wid)])
+    except ValueError:
+        print('Dimensions should be numbers! Try again...')
+        get_room_dims(loop)
+
+def get_security_dist():
+    try:
+        security = input('What is the security distance?: ')
+        return float(security)
+    except ValueError:
+        print('Dimensions should be numbers! Try again...')
+        get_security_dist()
+
+
+def get_table_dims():
+    try:
+        _len = input('What is the lenght of the table?: ')
+        _wid = input('What is the width of the table?: ')
+        return [float(_len), float(_wid)]
+    except ValueError:
+        print('Dimensions should be numbers! Try again...')
+        get_table_dims()
