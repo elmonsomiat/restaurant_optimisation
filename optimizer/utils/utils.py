67351ebf63e4c4a3e3a318874081bcc686a66fe6
elmonsomiat <asolaguren33@hotmail.com>
2020-07-06 20:25:22 +0100

    updated core print funcs

--- optimizer/utils/utils.py
+++ optimizer/utils/utils.py
@@ -16,32 +16,37 @@ def calculate_rectangles(room_size, security_distance, table_sizes):
     options = resize_rectangles(table_sizes, security_distance)
     rectangles = repeat_rectangles(options, room_size)[0:-3]
     return rectangles
-
+    
 
 def get_room_dims(loop):
-    _len = input(f'What is the lenght of space {loop}?: ')
-    _wid = input(f'What is the width of space {loop}?: ')
-    try:
-        return [float(_len), float(_wid)]
-        restaurant_list.append([float(_len), float(_wid)])
-    except ValueError:
-        print('Dimensions should be numbers! Try again...')
-        get_room_dims(loop)
+    while True:
+        try:
+            _len = input(f'What is the lenght of space {loop}?: ')
+            _wid = input(f'What is the width of space {loop}?: ')
+            return [float(_len), float(_wid)]
+        except ValueError:
+            print('Dimensions should be numbers! Try again...')
+        else:
+            break
+    
 
 def get_security_dist():
-    try:
-        security = input('What is the security distance?: ')
-        return float(security)
-    except ValueError:
-        print('Dimensions should be numbers! Try again...')
-        get_security_dist()
-
+    while True:
+        try:
+            security = input('What is the security distance?: ')
+            return float(security)
+        except ValueError:
+            print('Dimensions should be numbers! Try again...')
+        else:
+            break
 
 def get_table_dims():
-    try:
-        _len = input('What is the lenght of the table?: ')
-        _wid = input('What is the width of the table?: ')
-        return [float(_len), float(_wid)]
-    except ValueError:
-        print('Dimensions should be numbers! Try again...')
-        get_table_dims()
+    while True:
+        try:
+            _len = input('What is the lenght of the table?: ')
+            _wid = input('What is the width of the table?: ')
+            return [float(_len), float(_wid)]
+        except ValueError:
+            print('Dimensions should be numbers! Try again...')
+        else:
+            break
